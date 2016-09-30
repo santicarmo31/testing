c3292f55f4e8b6f318e6c022b63b054500bcea40
santiago carmona gonzalez <santicarmo@santiagos-macbook-air.local>
2016-09-30 12:09:48 -0500

    Initial Commit

---
+++ ExamenRecetario/ViewController.swift
@@ -0,0 +1,28 @@
+//
+//  ViewController.swift
+//  ExamenRecetario
+//
+//  Created by Carrancá on 28/09/16.
+//  Copyright © 2016 Santiago Carrancá. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    @IBAction func doneIngredientsOne(sender: AnyObject) {
+    dismissViewControllerAnimated(true, completion: nil)
+    }
+
+}
+
